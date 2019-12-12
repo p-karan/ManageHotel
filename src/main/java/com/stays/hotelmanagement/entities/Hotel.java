c6fed2fa6e7b835692f2de6b105b7a2a6a81aa2f
p-karan <praja.karan@gmail.com>
2019-12-12 20:19:09 +0530

    Added Hotel and Room entities with services and controller

---
+++ src/main/java/com/stays/hotelmanagement/entities/Hotel.java
@@ -0,0 +1,52 @@
+package com.stays.hotelmanagement.entities;
+
+import lombok.*;
+import javax.persistence.*;
+import java.sql.Blob;
+import java.util.Set;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="HBMS_HOTELS")
+public class Hotel{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(length = 6)
+    private int hotelId;
+
+    @Column(nullable = false, length = 100)
+    private String hotelName;
+
+    @Embedded
+    @Column(nullable = false)
+    private Address address;
+
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name="hotelId", nullable = false)
+    private Set<Room> rooms;
+
+    @Column(nullable = false , precision = 2, scale = 1, columnDefinition = "FLOAT DEFAULT 0.0")
+    private double rating;
+
+    @Column(nullable = false, length = 400)
+    private String hotelDescription;
+
+    @Lob
+    private Blob hotelImage;
+
+    @Column(nullable = false, columnDefinition = "INT DEFAULT 0",length = 2)
+    private int discount;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private HotelStatus hotelOperationalStatus;
+
+    public enum HotelStatus {
+        OPEN, CLOSED
+    }
+}
